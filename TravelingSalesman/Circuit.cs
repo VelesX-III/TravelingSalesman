6b7579a5d97e56ace762818738020b2b323687af
Sephiran <robertothomas427@gmail.com>
2020-12-12 19:07:57 -0800

    Re-purposed logic from project 3. Need proper crossing operation.

--- TravelingSalesman/Circuit.cs
+++ TravelingSalesman/Circuit.cs
@@ -18,7 +18,6 @@ namespace TravelingSalesman
                 double value = 0;
                 for (int i = 0; i < Path.Count; i++)
                 {
-                    Console.WriteLine(Path[i].State + "~" + Path[(i + 1) % Path.Count].State);
                     value += Math.Sqrt(Math.Pow(Path[(i + 1) % Path.Count].Latitude - Path[i].Latitude, 2) + Math.Pow(Path[(i + 1) % Path.Count].Longitude - Path[i].Longitude, 2));
                 }
                 return value;
@@ -27,6 +26,13 @@ namespace TravelingSalesman
         public Circuit()
         {
             Path = JsonConvert.DeserializeObject<List<City>>(File.ReadAllText(@"C:\Users\Administrator\source\repos\TravelingSalesman\TravelingSalesman\Capitals.json")).Permute();
+        }
+        public void PrintPath()
+        {
+            for (int i = 0; i < Path.Count; i++)
+            {
+                Console.WriteLine(Path[i].State + "~" + Path[(i + 1) % Path.Count].State);
+            }
         }
     }
     public struct City
--- TravelingSalesman/Program.cs
+++ TravelingSalesman/Program.cs
@@ -8,9 +8,45 @@ namespace TravelingSalesman
     {
         static void Main(string[] args)
         {
-            Circuit circuit = new Circuit();
-            circuit.Path.ForEach(x => Console.WriteLine(x.State));
-            Console.WriteLine(circuit.Distance);
+            List<Circuit> individuals = new List<Circuit>();
+            const int population = 100;
+            const int iterations = 100;
+            const double mutationRate = .1;
+            Random random = new Random();
+            for (int i = 0; i < population; i++)
+            {
+                individuals.Add(new Circuit());
+            }
+            individuals = individuals.OrderBy(c => c.Distance).ToList();
+
+            for (int i = 0; i < iterations; i++)
+            {
+                List<Circuit> children = new List<Circuit>();
+                for (int j = 0; j < individuals.Count - 1; j += 2)
+                {
+                    //Create children from two parents.
+                    List<City>[] childPaths = { new List<City>(), new List<City>() };
+                    childPaths[0].AddRange(individuals[j].Path.GetRange(0, 27));
+                    childPaths[0].AddRange(individuals[j + 1].Path.GetRange(27, 26));
+                    childPaths[1].AddRange(individuals[j].Path.GetRange(27, 26));
+                    childPaths[1].AddRange(individuals[j + 1].Path.GetRange(0, 27));
+                    //Roll for mutation.
+                    if (random.NextDouble() < mutationRate)
+                    {
+                        childPaths[0].Permute();
+                    }
+                    if (random.NextDouble() < mutationRate)
+                    {
+                        childPaths[1].Permute();
+                    }
+                    children.Add(new Circuit() { Path = childPaths[0] });
+                    children.Add(new Circuit() { Path = childPaths[1] });
+                }
+                individuals.AddRange(children);
+                individuals = individuals.OrderBy(c => c.Distance).Take(population).ToList();
+                Console.WriteLine(individuals.First().Distance);
+            }
+            individuals.First().PrintPath();
         }
     }
 }
