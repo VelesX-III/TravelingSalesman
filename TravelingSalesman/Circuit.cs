5811f4799e4a455d1c09357f8db97bcd37651468
Sephiran <robertothomas427@gmail.com>
2020-12-14 05:32:45 -0800

    Crossover function done.

--- TravelingSalesman/Circuit.cs
+++ TravelingSalesman/Circuit.cs
@@ -8,9 +8,18 @@ using Newtonsoft.Json;
 
 namespace TravelingSalesman
 {
+    /// <summary>
+    /// Represents a potential solution to the problem.
+    /// </summary>
     public class Circuit
     {
-        public List<City> Path { get; set; }
+        /// <summary>
+        /// The order in which each <see cref="City"/> is visited.
+        /// </summary>
+        public List<City> Path { get; private set; }
+        /// <summary>
+        /// Gets the total distance traveled over the <see cref="Path"/>.
+        /// </summary>
         public double Distance
         {
             get
@@ -23,10 +32,33 @@ namespace TravelingSalesman
                 return value;
             }
         }
+        /// <summary>
+        /// Constructs a new <see cref="Circuit"/> with an empty <see cref="Path"/>.
+        /// </summary>
         public Circuit()
         {
-            Path = JsonConvert.DeserializeObject<List<City>>(File.ReadAllText(@"C:\Users\Administrator\source\repos\TravelingSalesman\TravelingSalesman\Capitals.json")).Permute();
+            Path = new List<City>();
         }
+        /// <summary>
+        /// Constructs a new <see cref="Circuit"/> from a collection that can be cast as the <see cref="Path"/>.
+        /// </summary>
+        /// <param name="path">A collection representing the path.</param>
+        public Circuit(IEnumerable<City> path)
+        {
+            Path = path.ToList();
+        }
+        /// <summary>
+        /// Initializes the <see cref="Circuit"/> with a random <see cref="Path"/>.
+        /// </summary>
+        /// <returns>A reference to the initialized <see cref="Circuit"/>.</returns>
+        public Circuit Initialize()
+        {
+            Path = JsonConvert.DeserializeObject<List<City>>(File.ReadAllText(@"C:\Users\Administrator\source\repos\TravelingSalesman\TravelingSalesman\Capitals.json")).Permute();
+            return this;
+        }
+        /// <summary>
+        /// Prints the path traveled on standard out.
+        /// </summary>
         public void PrintPath()
         {
             for (int i = 0; i < Path.Count; i++)
@@ -34,11 +66,62 @@ namespace TravelingSalesman
                 Console.WriteLine(Path[i].State + "~" + Path[(i + 1) % Path.Count].State);
             }
         }
+        /// <summary>
+        /// Produces a new <see cref="Circuit"/> whose <see cref="Path"/> is formed from those of its operands.
+        /// Every other <see cref="City"/> in the <see cref="Path"/> of the first operand is spliced in order with the remaining cities of the second.
+        /// </summary>
+        /// <param name="circuitA">The <see cref="Circuit"/> whose every other <see cref="City"/> will be present in the result <see cref="Path"/>.</param>
+        /// <param name="circuitB">The <see cref="Circuit"/> whose remaining cities will be inserted in order into the remaining null <see cref="Path"/> indices.</param>
+        /// <returns>A <see cref="Circuit"/> with a <see cref="Path"/> that combines the paths of its operands.</returns>
+        /// <remarks>This operation is non-Abelian.</remarks>
+        public static Circuit operator *(Circuit circuitA, Circuit circuitB)
+        {
+            if (circuitA.Path.Count == 0 || circuitB.Path.Count == 0 || circuitA.Path.Count != circuitB.Path.Count)
+            {
+                throw new Exception("Paths incompatible for crossing.");
+            }
+
+            City[] cities = new City[circuitA.Path.Count];
+            List<City> remainder = circuitB.Path.ToList(); //Seems redundant, but forces copy-initialization.
+            for (int i = 0; i < circuitA.Path.Count; i += 2)
+            {
+                cities[i] = circuitA.Path[i];
+                remainder.Remove(circuitA.Path[i]);
+            }
+            Queue<City> queue = new Queue<City>();
+            remainder.ForEach(c => queue.Enqueue(c));
+            for (int i = 1; i < cities.Length; i += 2)
+            {
+                if (cities[i] is null)
+                {
+                    cities[i] = queue.Dequeue();
+                }
+            }
+
+            return new Circuit(cities);
+        }
     }
-    public struct City
+    /// <summary>
+    /// Represents a capital city as a vertex.
+    /// </summary>
+    public class City
     {
-        public string State;
-        public double Latitude;
-        public double Longitude;
+        /// <summary>
+        /// The state in the capital <see cref="City"/> lies in.
+        /// </summary>
+        public string State { get; set; }
+        /// <summary>
+        /// The latitude coordinate of the <see cref="City"/>.
+        /// </summary>
+        public double Latitude { get; set; }
+        /// <summary>
+        /// The longitude coordinate of the <see cref="City"/>.
+        /// </summary>
+        public double Longitude { get; set; }
+        /// <summary>
+        /// Represents the <see cref="City"/> as a string.
+        /// </summary>
+        /// <returns>The <see cref="State"/> as a string.</returns>
+        public override string ToString() => State;
     }
 }
--- TravelingSalesman/Program.cs
+++ TravelingSalesman/Program.cs
@@ -8,45 +8,51 @@ namespace TravelingSalesman
     {
         static void Main(string[] args)
         {
-            List<Circuit> individuals = new List<Circuit>();
-            const int population = 100;
-            const int iterations = 100;
-            const double mutationRate = .1;
-            Random random = new Random();
-            for (int i = 0; i < population; i++)
-            {
-                individuals.Add(new Circuit());
-            }
-            individuals = individuals.OrderBy(c => c.Distance).ToList();
+            Circuit circuit1 = new Circuit().Initialize();
+            Circuit circuit2 = new Circuit().Initialize();
 
-            for (int i = 0; i < iterations; i++)
-            {
-                List<Circuit> children = new List<Circuit>();
-                for (int j = 0; j < individuals.Count - 1; j += 2)
-                {
-                    //Create children from two parents.
-                    List<City>[] childPaths = { new List<City>(), new List<City>() };
-                    childPaths[0].AddRange(individuals[j].Path.GetRange(0, 27));
-                    childPaths[0].AddRange(individuals[j + 1].Path.GetRange(27, 26));
-                    childPaths[1].AddRange(individuals[j].Path.GetRange(27, 26));
-                    childPaths[1].AddRange(individuals[j + 1].Path.GetRange(0, 27));
-                    //Roll for mutation.
-                    if (random.NextDouble() < mutationRate)
-                    {
-                        childPaths[0].Permute();
-                    }
-                    if (random.NextDouble() < mutationRate)
-                    {
-                        childPaths[1].Permute();
-                    }
-                    children.Add(new Circuit() { Path = childPaths[0] });
-                    children.Add(new Circuit() { Path = childPaths[1] });
-                }
-                individuals.AddRange(children);
-                individuals = individuals.OrderBy(c => c.Distance).Take(population).ToList();
-                Console.WriteLine(individuals.First().Distance);
-            }
-            individuals.First().PrintPath();
+            Circuit circuit3 = circuit1 * circuit2;
+            Console.WriteLine(circuit3.Path.Count);
+
+            //List<Circuit> individuals = new List<Circuit>();
+            //const int population = 100;
+            //const int iterations = 100;
+            //const double mutationRate = .1;
+            //Random random = new Random();
+            //for (int i = 0; i < population; i++)
+            //{
+            //    individuals.Add(new Circuit().Initialize());
+            //}
+            //individuals = individuals.OrderBy(c => c.Distance).ToList();
+
+            //for (int i = 0; i < iterations; i++)
+            //{
+            //    List<Circuit> children = new List<Circuit>();
+            //    for (int j = 0; j < individuals.Count - 1; j += 2)
+            //    {
+            //        //Create children from two parents.
+            //        List<City>[] childPaths = { new List<City>(), new List<City>() };
+            //        childPaths[0].AddRange(individuals[j].Path.GetRange(0, 27));
+            //        childPaths[0].AddRange(individuals[j + 1].Path.GetRange(27, 26));
+            //        childPaths[1].AddRange(individuals[j].Path.GetRange(27, 26));
+            //        childPaths[1].AddRange(individuals[j + 1].Path.GetRange(0, 27));
+            //        //Roll for mutation.
+            //        if (random.NextDouble() < mutationRate)
+            //        {
+            //            childPaths[0].Permute();
+            //        }
+            //        if (random.NextDouble() < mutationRate)
+            //        {
+            //            childPaths[1].Permute();
+            //        }
+            //        children.Add(new Circuit() { Path = childPaths[0] });
+            //        children.Add(new Circuit() { Path = childPaths[1] });
+            //    }
+            //    individuals.AddRange(children);
+            //    individuals = individuals.OrderBy(c => c.Distance).Take(population).ToList();
+            //    Console.WriteLine(individuals.First().Distance);
+            //}
+            //individuals.First().PrintPath();
         }
     }
 }
